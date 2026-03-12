ad1dc7c3cb6a1b88bf47f4518966d1e00d2e9867
Daria <dashacramb@mail.ru>
2026-03-12 22:40:50 +0300

    helloo

--- 4-exp.py
+++ 4-exp.py
@@ -1,10 +1,37 @@
 sum_input = input("Введите сумму: ")
-ignore = sum_input.lower()
-ignore2 = ignore.split(".")
-rub, kop = ignore2
 
-if len(kop) > 2:
+# Убедимся, что входная строка содержит "руб"
+if "руб" not in sum_input:
     print("Некорректный формат суммы")
 else:
-    formatted_sum = f"{rub}.{kop:0<2}₽"
-    print(formatted_sum)
+    parts = sum_input.split()
+
+    # Проверим наличие индексов и корректность данных
+    if len(parts) >= 2 and 'руб' in parts:
+        rub_index = parts.index('руб')
+
+        # Попробуем преобразовать рубли в число
+        if rub_index > 0 and parts[rub_index - 1].isdigit():
+            rub = int(parts[rub_index - 1])
+        else:
+            print("Некорректный формат суммы")
+            exit()
+
+        # Проверяем наличие копеек и их правильность
+        if 'коп' in parts:
+            kop_index = parts.index('коп')
+            if kop_index > rub_index and parts[kop_index - 1].isdigit():
+                kop = int(parts[kop_index - 1])
+            else:
+                print("Некорректный формат суммы")
+                exit()
+        else:
+            kop = 0
+
+        # Проверка на корректность копеек
+        if 0 <= kop <= 99:
+            print(f"{rub}.{kop:02} ₽")
+        else:
+            print("Некорректный формат суммы")
+    else:
+        print("Некорректный формат суммы")
